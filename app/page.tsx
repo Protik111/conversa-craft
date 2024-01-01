b604883f59897ffb4d2c9aa0b514e3118409ab97
Rafiur Rahman Protik <rafiurprotik111@gmail.com>
2024-01-01 12:37:00 +0600

    [DONE] basic authentication with protected route

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,18 @@
-export default function Home() {
+import { getServerSession } from "next-auth"
+
+export default async function Home() {
+  const session = await getServerSession();
+
   return (
     <main>
-      <h1>Hello</h1>
+      {/* <p>Get Server Session</p>
+      {
+        session?.user?.name ? (
+          <div>{session?.user.name}</div>
+        ) : (
+          <div>No logged in</div>
+        )
+      } */}
     </main>
   )
 }
